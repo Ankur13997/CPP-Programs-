a69365d665507b8c72bc5ddfb0066560c0dcc42d
Ankur13997 <54276967+Ankur13997@users.noreply.github.com>
2021-10-02 01:11:22 +0530

    Solved using 1D dp in O(n)

--- TIme to buy stock.cpp
+++ TIme to buy stock.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int minPrice = INT_MAX;
-        int maxProf = 0;
-        for(int i = 0; i < prices.size(); i++)
+   int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+
+        
+        
+        int mini=prices[0];
+        int maxp=0;
+        for(int i=0;i<n;i++)
         {
-            minPrice = min(minPrice, prices[i]);
-            maxProf = max(maxProf, prices[i] - minPrice);
+            if(mini<prices[i])
+            {
+                maxp=max(maxp,prices[i]-mini);
+            }
+            else 
+                mini=prices[i];
+            
         }
-        return maxProf;
+        return maxp;
     }
 };
 
